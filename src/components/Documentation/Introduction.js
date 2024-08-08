2fd4dd7de8a19970e02fcd900d932a2f8f8823cf
panugothrakesh <rakeshpanugoth2003@gmail.com>
2024-08-08 14:06:31 +0530

    Last

--- src/components/Documentation/Introduction.js
+++ src/components/Documentation/Introduction.js
@@ -8,7 +8,7 @@ function Introduction() {
             <img src="logo.jpg" alt="" className='h-8 rounded-md'/>
             <h1 className='text-4xl font-bold'>Catalog Assignment</h1>
         </div>
-        <p className='text-lg text-[#6F7177] leading-8 md:w-3/4 w-full'>As part of the interview process, I was assigned a task to convert a provided Figma design into a functional ReactJS component using Recharts and API to fetch the Data. The goal was to accurately replicate the visual design and ensure the component is interactive.</p>
+        <p className='text-lg text-[#6F7177] leading-8 md:w-3/4 w-full mt-8'>As part of the interview process, I was assigned a task to convert a provided Figma design into a functional ReactJS component using Recharts and API to fetch the Data. The goal was to accurately replicate the visual design and ensure the component is interactive.</p>
         <div className='w-full border mt-8'></div>
     </div>
 
